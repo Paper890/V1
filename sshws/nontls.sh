597447e7725e052f2574e859363faaa6bfa44a85
Paper890 <140361640+Paper890@users.noreply.github.com>
2024-08-06 19:10:24 +0800

    Update nontls.sh

--- sshws/nontls.sh
+++ sshws/nontls.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
-# Proxy For NO TLS
-# ==========================================
-# Color
-RED='\033[0;31m'
-NC='\033[0m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT='\033[0;37m'
-# ==========================================
-clear
-red='\e[1;31m'
-green='\e[0;32m'
-yell='\e[1;33m'
-NC='\e[0m'
-echo "Installing SSH Websocket None TLS by SAN " | lolcat
-echo "Progress..." | lolcat
-sleep 3
+echo "Installing SSH Websocket None TLS by SAN "
 
 # Getting Proxy Template
-wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/Paper890/mysc/main/websocket/ws-nontls.py
+wget -q -O /usr/local/bin/ws-nontls https://raw.githubusercontent.com/Paper890/V1/main/sshws/ws-nontls.py
 chmod +x /usr/local/bin/ws-nontls
 
 # Installing Service
